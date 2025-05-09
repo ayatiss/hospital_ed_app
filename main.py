2f6afc1383f26186ef7642d9269300b7538a6558
ayatiss <tiseya280@gmail.com>
2025-05-09 18:18:54 +0100

    Add project files2

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, Depends
 from app.core.database import SessionLocal
 
+from app.core.database import Base, engine
+from app.models import user  # make sure your model is imported
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
